5593c2b95489be2e134a043bd1ccd5b0aac00d43
odalys <odalys.icaza8785@est.uca.edu.ni>
2021-05-12 13:34:20 -0600

    Implementando manejo offline

--- app/src/main/java/com/example/apidog/repository/DogRepository.kt
+++ app/src/main/java/com/example/apidog/repository/DogRepository.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.lang.Exception
+import java.net.UnknownHostException
 
 class DogRepository constructor (
     private val dogDao: DogDao,
@@ -23,6 +24,7 @@ class DogRepository constructor (
         emit(DataState.Loading)
         delay(5000)
         try {
+            //throw UnknownHostException()
             val dogData = dogRetrofit.get()
             val dogMap = networkMapper.mapFromEntityList(dogData)
             for (tempDog in dogMap){
@@ -31,6 +33,17 @@ class DogRepository constructor (
             val cacheDog = dogDao.get()
             emit(DataState.Success(cacheMapper.mapFromEntityList(cacheDog)))
         }catch (e: Exception){
+            when(e){
+               is UnknownHostException->{
+                   val cacheDog = dogDao.get()
+                   if (cacheDog.isEmpty()) {
+                       emit(DataState.Error(java.lang.Exception("Tabla de perritos vacía :( conectese a internet para poblar la tabla!!")))
+                   }else{
+                       emit(DataState.Success(cacheMapper.mapFromEntityList(cacheDog)))
+                   }
+               }
+
+            }
             emit(DataState.Error(e))
         }
     }
